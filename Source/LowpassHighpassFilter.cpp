b8757ac577eb23beb540bbc1ab529c05a9544b16
Your Name <you@example.com>
2024-02-20 11:34:41 +0100

    feat: creating the process block

--- Source/LowpassHighpassFilter.cpp
+++ Source/LowpassHighpassFilter.cpp
@@ -22,11 +22,32 @@ void LowpassHighpassFilter::setCutoffFrequency(float cutoffFrequency)
 
 void LowpassHighpassFilter::setSamplingRate(float samplingRate)
 {
-	this->samplingRate = samplingRate;
+	this->samplingRate = samplingRate;	
 }
 
 void LowpassHighpassFilter::processBlock(juce::AudioBuffer<float>& buffer,
 											juce::MidiBuffer& midiMessages)
-{
-	// Process audio buffer
+{	
+	constexpr auto PI = 3.14159265359f;
+	dnBuffer.resize(buffer.getNumChannels(), 0.f);
+    auto sign = highpass ? -1.f : 1.f;
+
+    for (auto channel = 0; channel < buffer.getNumChannels(); ++channel) {
+
+        auto channelSamples = buffer.getWritePointer(channel);
+
+        for (auto i = 0; i < buffer.getNumSamples(); ++i) {
+            const auto tan = std::tan(PI * cutoffFrequency / samplingRate);
+
+            const auto a1 = (tan - 1.f) / (tan + 1.f);
+            const auto inputSample = channelSamples[i];
+            const auto allpassFilteredSample = a1 * inputSample + dnBuffer[channel];
+            dnBuffer[channel] = inputSample - a1 * allpassFilteredSample;
+
+            const auto filterOutput =
+                0.5f * (inputSample + sign * allpassFilteredSample);
+
+            channelSamples[i] = filterOutput;
+        }
+    }
 }
